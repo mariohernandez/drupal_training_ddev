920a8fe4b028ca2690fd4e20ac6d6833871f9e51
Mario Hernandez <mario.hernandez@mediacurrent.com>
2020-05-29 23:18:56 -0700

    ignores sites dir

--- sites/default/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-
-// #ddev-generated: Automatically generated Drupal settings file.
-if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php')) {
-  include $app_root . '/' . $site_path . '/settings.ddev.php';
-}
