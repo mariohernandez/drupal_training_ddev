26cad668adf3e240bf686d2b7f8aa0a0be284d6d
Mario Hernandez <mario.hernandez@mediacurrent.com>
2020-06-06 18:10:58 -0700

    Adds settings.ddev

---
+++ sites/default/settings.ddev.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * #ddev-generated: Automatically generated Drupal settings file.
+ * ddev manages this file and may delete or overwrite the file unless this
+ * comment is removed.
+ */
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+
+$host = "db";
+$port = 3306;
+
+// If DDEV_PHP_VERSION is not set but IS_DDEV_PROJECT *is*, it means we're running (drush) on the host,
+// so use the host-side bind port on docker IP
+if (empty(getenv('DDEV_PHP_VERSION') && getenv('IS_DDEV_PROJECT') == 'true')) {
+  $host = "127.0.0.1";
+  $port = 32775;
+}
+
+$databases['default']['default'] = array(
+  'database' => "db",
+  'username' => "db",
+  'password' => "db",
+  'host' => $host,
+  'driver' => "mysql",
+  'port' => $port,
+  'prefix' => "",
+);
+
+$settings['hash_salt'] = 'lBlApISKMttCEygQfMkWcJAgmjmGZmBpUNletlhYBLoguPvAVCYnXujqIbOFsYnW';
+
+// This will prevent Drupal from setting read-only permissions on sites/default.
+$settings['skip_permissions_hardening'] = TRUE;
+
+// This will ensure the site can only be accessed through the intended host
+// names. Additional host patterns can be added for custom configurations.
+$settings['trusted_host_patterns'] = ['.*'];
+
+// Don't use Symfony's APCLoader. ddev includes APCu; Composer's APCu loader has
+// better performance.
+$settings['class_loader_auto_detect'] = FALSE;
+
+// This specifies the default configuration sync directory.
+// $config_directories (pre-Drupal 8.8) and
+// $settings['config_sync_directory'] are supported
+// so it should work on any Drupal 8 or 9 version.
+if (defined('CONFIG_SYNC_DIRECTORY') && empty($config_directories[CONFIG_SYNC_DIRECTORY])) {
+  $config_directories[CONFIG_SYNC_DIRECTORY] = 'sites/default/files/sync';
+}
+elseif (empty($settings['config_sync_directory'])) {
+  $settings['config_sync_directory'] = 'sites/default/files/sync';
+}
